68ebaccde009691d142c0c6cdc8a37b3c4149781
JK-121 <kyeongmin719@gmail.com>
2021-08-03 19:09:21 +0900

    JS 다시보기1

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("dsfdsf");
+
+let a = 5;
+const b = 2;
+let myName = "jk";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+myName = "jkm";
+
+console.log("My new name is " + myName);
